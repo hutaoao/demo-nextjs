6af1a31651dcec27f7d4e8e9ebf9be72651b04ff
hutao <hutao@weherepost.com>
2024-06-11 15:19:01 +0800

    feat: 添加代理

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/apis/:path*',
+        destination: `https://hutaoao.cn:7001/:path*`, // 替换为你的后端服务URL
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
